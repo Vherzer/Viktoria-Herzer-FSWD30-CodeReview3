8035f34bde4834cb00fdfb06c0af190012db06ae
viktoria Herzer <viktoria.herzer@gmail.com>
2017-12-10 00:47:57 +0100

    updated nearly complete, maybe i can still figure out the input to work in the function

--- codereview3_script.js
+++ codereview3_script.js
@@ -3,10 +3,10 @@
 function calculateinsurance(){
 	var name="John Doe";
 	var age=20;
-
 	var country="Austria";
 	var horsepower=150;
 	var insurance=Math.round((horsepower*100)/(age+150));
-	document.getElementById("result").innerHTML = insurance + "€";
+	document.getElementById("result").innerHTML = "Your insurance quote is " + insurance + "€";
 }
-console.log(document.getElementById('age').value);
+
+/*console.log(calculateinsurance());*/
